4afa210e6bee6f4aa45b4fe0f67484bfc685375a
yloukou <yloukou@solocal.com>
2023-06-02 11:49:15 +0200

    2nd Commit & push

---
+++ Yanager/src/layouts/default/AppBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar :elevation="2" flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+      Yanager
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
